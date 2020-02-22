a5ec2e5f4961207da67db2ff19833dac95911237
Lê Hoàng Luật <dr.goodperry@gmail.com>
2020-02-22 19:39:00 +0700

    logic bao cao

---
+++ QLPhongMachTu/QLPhongMachTuBUS/BaoCaoBUS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using QLPhongMachTuDAO;
+using QLPhongMachTuDTO;
+
+namespace QLPhongMachTuBUS
+{
+    public class BaoCaoBUS
+    {
+        BaoCaoDAO dao = new BaoCaoDAO();
+
+        public DataTable BaoCao_DoanhThu_Month(DateTime _month)
+        {
+            return dao.BaoCao_DoanhThu_Month(_month);
+        }
+        public DataTable BaoCao_SuDungThuoc_Month(DateTime _month)
+        {
+            return dao.BaoCao_SuDungThuoc_Month(_month);
+        }
+
+    }
+}
---
+++ QLPhongMachTu/QLPhongMachTuDAO/BaoCaoDAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Provider;
+using QLPhongMachTuDTO;
+
+namespace QLPhongMachTuDAO
+{
+    public class BaoCaoDAO
+    {
+        public DataTable BaoCao_DoanhThu_Month(DateTime _month)
+        {
+            DataProvider db = new DataProvider();
+            return db.ReadDataAddPram("SP_ReadBaoCao_DoanhThu_Month", new string[1] { "@month" }
+                                                         , new object[1] { _month }, 100);
+        }
+
+        public DataTable BaoCao_SuDungThuoc_Month(DateTime _month)
+        {
+            DataProvider db = new DataProvider();
+            return db.ReadDataAddPram("SP_ReadBaoCao_SuDungThuoc_Month", new string[1] { "@month" }
+                                                         , new object[1] { _month }, 100);
+        }
+    }
+}
